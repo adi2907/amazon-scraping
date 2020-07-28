bf0dd8722eb5b096d5fb2e9664d9ed34d771bb1c
Vijay <kuriringohankamehameha@github.com>
2020-07-28 21:04:12 +0530

    Add preliminary scripts

---
+++ parse_html.py
@@ -0,0 +1,495 @@
+from bs4 import BeautifulSoup
+import os
+import re
+import pickle
+
+
+def init_parser(category: str):
+    if not os.path.exists(os.path.join(os.getcwd(), 'data', f'{category}.html')):
+        raise ValueError(f'HTML file for category:{category} missing. Please make sure that it is downloaded')
+
+    with open(os.path.join('data', f'{category}.html'), 'rb') as f:
+        html_text = f.read()
+        soup = BeautifulSoup(html_text, 'html.parser')
+    
+    return soup
+
+
+def get_product_id(url):
+    if url.startswith("/gp/slredirect"):
+        # Only match until the first % (?)
+        # Ignore the first 2 charactes (2F)
+        pattern = r'/gp/slredirect/.+dp%..(.+?)%.+'
+    else:
+        pattern = r'/.+/dp/(.+)/.+$'
+    match = re.match(pattern, url)
+    product_id = None
+    if match and len(match.groups()) > 0:
+        product_id = match.groups()[0]
+    return product_id
+
+
+def get_product_mapping(soup) -> dict:
+    product_info = soup.find_all("a", class_='a-link-normal a-text-normal')
+
+    product_map = dict()
+
+    for product_details in product_info:
+        url = product_details['href']
+        match = re.match(r'/.+/dp/(.+)/.+$', url)
+        if match and len(match.groups()) > 0:
+            product_id = match.groups()[0]
+            product_map[product_id] =  product_details.text.strip()
+    
+    return product_map
+
+
+def get_product_info(soup, base_url="https://amazon.in"):
+    """Fetches the product details for all the products for a single page
+    """
+    product_bars = soup.find_all("div", class_="sg-col-inner")
+    
+    product_info = dict()
+    
+    for product_bar in product_bars:
+        title_node = product_bar.find("a", class_="a-link-normal a-text-normal")
+        
+        if title_node is None:
+            # Invalid node. Leave this
+            continue
+        
+        title = title_node.find("span").text.strip()
+        
+        if title in product_info:
+            # We've already covered this product
+            continue
+        
+        product_info[title] = dict()
+
+        if title_node is not None and 'href' in title_node.attrs:
+            product_info[title]['product_url'] = title_node.attrs['href']
+            product_info[title]['product_id'] = get_product_id(title_node.attrs['href'])
+        else:
+            product_info[title]['product_url'] = None
+            product_info[title]['product_id'] = None
+        
+        rating_row = product_bar.find("div", class_="a-row a-size-small")
+        if rating_row is not None:
+            for idx, row in enumerate(rating_row.find_all("span")):
+                if idx == 0:
+                    avg_rating = row.attrs['aria-label'].strip()
+                else:
+                    if 'aria-label' in row.attrs:
+                        total_ratings = row.attrs['aria-label'].strip()
+        else:
+            avg_rating, total_ratings = None, None
+        
+        
+        product_info[title]['avg_rating'] = avg_rating
+        product_info[title]['total_ratings'] = total_ratings
+
+        symbol = product_bar.find("span", class_="a-price-symbol")
+        price = product_bar.find("span", class_="a-price-whole")
+
+        if price is not None:
+            if symbol is not None:
+                product_info[title]['price'] = symbol.text.strip() + price.text.strip()
+            else:
+                product_info[title]['price'] = price.text.strip()
+        else:
+            product_info[title]['price'] = None
+
+        # If the price is reduced, get the old price as well
+        old_price = product_bar.find("span", class_="a-price a-text-price")
+        if old_price is not None:
+            product_info[title]['old_price'] = old_price.find("span", class_="a-offscreen").text.strip()
+        else:
+            product_info[title]['old_price'] = None
+        
+        # Check if this item is currently deliverable using secondary pricing information
+        secondary_information = product_bar.find("span", class_="a-color-price")
+        if secondary_information is None:
+            product_info[title]['secondary_information'] = None
+        else:
+            product_info[title]['secondary_information'] = secondary_information.text.strip()
+        
+        # Get the image information
+        img_node = product_bar.find("img", class_="s-image")
+        if img_node is not None:
+            product_info[title]['image'] = img_node.attrs['src']
+        else:
+            product_info[title]['image'] = None
+        
+    return product_info
+
+
+def get_review_elements(soup):
+    target_class = "a-popover-trigger a-declarative"
+    js_elements = soup.find_all("a", class_=target_class)
+    return js_elements
+
+
+def get_reviews(soup):
+    target_id = "histogramTable"
+    results = soup.find(id=target_id)
+    reviews = results.find_all("span", class_="a-size-base")[1:]
+    five_star_reviews = reviews[0]
+    other_reviews = reviews[2::2]
+    results = [five_star_reviews.text.strip()] + [review.text.strip() for review in other_reviews]
+    return results
+
+
+def get_product_data(soup):
+    """Scrapes the Individual product detail page for a particular product
+    """
+    results = dict()
+    center_col_node = soup.find("div", id="centerCol")
+    if center_col_node is None:
+        raise ValueError("Product Detail Not Found")
+    
+    # Product Title
+    title = center_col_node.find("span", id="productTitle")
+    if title is None:
+        results['product_title'] = None
+    else:
+        results['product_title'] = title.text.strip()
+    
+    # Byline Information (Brand, etc)
+    byline_info = center_col_node.find("a", id="bylineInfo")
+    if byline_info is None:
+        results['byline_info'] = None
+    else:
+        results['byline_info'] = dict()
+        results['byline_info']['info'] = byline_info.text.strip()
+        results['byline_info']['url'] = byline_info.attrs['href'] if 'href' in byline_info.attrs else None
+    
+    # Rating Information
+    num_reviews = center_col_node.find("span", id="acrCustomerReviewText")
+    if num_reviews is None:
+        results['num_reviews'] = None
+    else:
+        results['num_reviews'] = num_reviews.text.strip()
+    
+    # Answered Questions
+    qanda_node = center_col_node.find("div", id="ask_feature_div")
+    if qanda_node is None:
+        results['answered_questions'] = None
+    else:
+        node = qanda_node.find("a", id="askATFLink")
+        results['answered_questions'] = node.span.text.strip() if node and hasattr(node, 'span') else None
+    
+    # Price
+    price_node = center_col_node.find("span", id="priceblock_ourprice")
+    if price_node is None:
+        price_node = center_col_node.find("span", id="priceblock_dealprice")
+        if price_node is None:
+            results['curr_price'] = None
+        else:
+            # Encoding and then again decoding to remove the Rupee Symbol
+            results['curr_price'] = price_node.text.strip().encode('ascii', 'ignore').decode('utf-8')
+    else:
+        results['curr_price'] = price_node.text.strip().encode('ascii', 'ignore').decode('utf-8')
+    
+    # Feature Points
+    feature_node = center_col_node.find("div", id="feature-bullets")
+    if feature_node is None:
+        results['features'] = None
+    else:
+        contents = feature_node.find_all(recursive=True)
+        features = []
+        for content in contents:
+            if hasattr(content, 'text'):
+                # Filter to remove the empty strings during string.split('\n)
+                features.extend(filter(None, content.text.strip().split('\n')))
+        results['features'] = features
+
+    # Offers
+    offers_node = soup.find("div", id="sopp_feature_div")
+    if offers_node is None:
+        results['offers'] = None
+    else:
+        offers = []
+        contents = offers_node.find_all(recursive=True)
+        for content in contents:
+            if hasattr(content, 'attrs') and 'href' in content.attrs and content.attrs['href'] == 'javascript:void(0)':
+                pass
+            else:
+                if hasattr(content, 'text'):
+                    offers.extend(filter(None, content.text.strip().split('\n')))
+        results['offers'] = offers
+    
+    # Description
+    description_node = soup.find("div", id="productDescription")
+    if description_node is None:
+        results['description'] = None
+    else:
+        contents = description_node.find_all(recursive=True)
+        description = []
+        for content in contents:
+            if hasattr(content, 'text'):
+                description.extend(filter(None, content.text.strip().split('\n')))
+        results['description'] = description
+    
+    # Product Details
+    detail_node = soup.find("div", id="prodDetails")
+    if detail_node is None:
+        results['product_details'] = None
+    else:
+        details = dict()
+        headers = detail_node.find_all("div", class_="secHeader")
+        if headers is not None:
+            for header in headers:
+                desc = header.text.strip()
+                details[desc] = dict()
+                # Per Table basis
+                table = detail_node.find("table")
+                if table is None:
+                    continue
+                labels, values = table.find_all("td", class_="label"), table.find_all("td", class_="value")
+                if len(labels) != len(values):
+                    continue
+                for label, value in zip(labels, values):
+                    _label, _value = label.text.strip(), value.text.strip()
+                    details[desc][_label] = _value
+
+        results['product_details'] = details
+    
+    # Customer Q&A
+    customer_node = soup.find("div", class_="askWidgetQuestions askLiveSearchHide")
+    if customer_node is None:
+        # Try to see if the lazy URL is loaded
+        customer_node = soup.find("div", class_="cdQuestionLazySeeAll")
+        if customer_node is not None:
+            url = customer_node.find("a").attrs['href']
+            results['customer_qa'] = url
+            # Mark it as lazy so that we can fetch it later
+            results['customer_lazy'] = True
+        else:
+            results['customer_qa'] = None
+    else:
+        # Let's fetch it now
+        # TODO: See how we can implement this
+        details = dict()
+        results['customer_qa'] = details
+    
+    # Customer Reviews
+    customer_reviews = soup.find("div", id="reviewsMedley")
+    if customer_reviews is None:
+        results['customer_reviews'] = None
+    else:
+        content = dict()
+        # Capture the Histogram first
+        histogram = customer_reviews.find("span", class_="cr-widget-Histogram")
+        if histogram is not None:
+            content['histogram'] = []
+            rows = histogram.find_all("tr", class_="a-histogram-row a-align-center")
+            for row in rows:
+                star = row.find("td", class_="aok-nowrap")
+                star = star.span.a.text.strip() # x stars
+                percent = row.find("td", class_="a-text-right a-nowrap")
+                percent = percent.find("a", class_="a-link-normal").text.strip()
+                data = {star: percent}
+                content['histogram'].append(data)
+        
+        # Now capture the reviews
+        reviews = customer_reviews.find_all("div", id=re.compile(r'customer_review-.+'))
+        if reviews is None:
+            content['reviews'] = None
+        else:
+            reviews_url = customer_reviews.find("div", id="reviews-medley-footer")
+            if reviews_url is not None:
+                reviews_url = reviews_url.find("a", {"data-hook": "see-all-reviews-link-foot"})
+                if reviews_url is not None:
+                    reviews_url = reviews_url.attrs['href']
+            content['reviews_url'] = reviews_url
+            
+            review_data = []
+            for review in reviews:
+                data = dict()
+                header = review.find("a", class_="a-link-normal")
+                if header is None:
+                    continue
+                
+                # Rating of the review
+                rating = header.attrs['title']
+                data['rating'] = rating
+                title = review.find("a", {"data-hook": "review-title"})
+                if title is not None:
+                    data['title'] = title.span.text.strip()
+                else:
+                    data['title'] = None
+                
+                # Review Date
+                date = review.find("span", {"data-hook": "review-date"})
+                if date is not None:
+                    date = date.text.strip()
+                data['review_date'] = date
+
+                # Review Body
+                regex = re.compile(r"<br/?>", re.IGNORECASE)
+                body = review.find("div", {"data-hook": "review-collapsed"})
+                if body is not None:
+                    body = re.sub(regex, '\n', str(body.span))
+                    body = body[6:-7] # Remove <span> and </span>
+                data['body'] = body
+
+                review_data.append(data)
+
+            content['reviews'] = review_data
+
+        results['customer_reviews'] = content
+
+    return results
+
+
+def get_qanda(soup):
+    """Parses the QandA page for a particular product
+    """
+    results = []
+    widgets = soup.find_all("div", class_="a-section askInlineWidget")
+    if widgets is not None:
+        for widget in widgets:
+            pairs = widget.find_all("div", class_="a-fixed-left-grid-col a-col-right")
+            if pairs is not None:
+                for pair in pairs:
+                    qanda = dict()
+                    pair = pair.find_all("div", class_="a-fixed-left-grid-col a-col-right")
+                    if pair is None:
+                        continue
+                    if len(pair) != 2:
+                        # We need to avoid the node which is repeated due to the recursive search
+                        # So we must proceed alternatively
+                        continue
+                    question = pair[0].find("a").span.text.strip()
+                    qanda['question'] = question
+                    answer = pair[1].span.text.strip()
+                    qanda['answer'] = answer
+                    results.append(qanda)
+    
+    # Get the url of the next page, if it exists
+    page_node = soup.find("ul", class_="a-pagination")
+    next_url = None
+    if page_node is not None:
+        next_url = page_node.find("li", class_="a-last")
+        if next_url is not None:
+            next_url = next_url.find("a")
+            if next_url is not None:
+                next_url = next_url.attrs['href']
+
+    return results, next_url
+
+
+def get_customer_reviews(soup, content={}):
+    # Now capture the reviews
+    reviews = soup.find_all("div", id=re.compile(r'customer_review-.+'))
+    if reviews is None:
+        content['reviews'] = None
+    else:
+        reviews_url = soup.find("div", id="reviews-medley-footer")
+        if reviews_url is not None:
+            reviews_url = reviews_url.find("a", {"data-hook": "see-all-reviews-link-foot"})
+            if reviews_url is not None:
+                reviews_url = reviews_url.attrs['href']
+        content['reviews_url'] = reviews_url
+        
+        review_data = []
+        for review in reviews:
+            data = dict()
+            header = review.find("a", class_="a-link-normal")
+            if header is None:
+                continue
+            
+            # Rating of the review
+            rating = header.attrs['title']
+            data['rating'] = rating
+            title = review.find("a", {"data-hook": "review-title"})
+            if title is not None:
+                data['title'] = title.span.text.strip()
+            else:
+                data['title'] = None
+            
+            # Review Date
+            date = review.find("span", {"data-hook": "review-date"})
+            if date is not None:
+                date = date.text.strip()
+            data['review_date'] = date
+
+            # Review about which product type
+            product_info = review.find("a", {"data-hook": "format-strip"})
+            if product_info is not None:
+                info = []
+                if hasattr(product_info, 'text'):
+                    info.append(product_info.text.strip())
+                product_info = product_info.find_all(recursive=True)
+                for content in product_info:
+                    if hasattr(content, 'text'):
+                        info.append(content.text.strip())
+                data['product_info'] = info
+            else:
+                data['product_info'] = None
+            
+            # Is this a verified purchase?
+            verified_purchase = review.find("span", {"data-hook": "avp-badge"})
+            if verified_purchase is None:
+                data['verified_purchase'] = False
+            else:
+                data['verified_purchase'] = True
+
+            # Review Body
+            # We need to convert all <br> tags to a newline, for identification
+            regex = re.compile(r"<br/?>", re.IGNORECASE)
+            body = review.find("div", {"data-hook": "review-collapsed"})
+            if body is None:
+                # We're at the main page, so it's no longer collapsed
+                body = review.find("span", {"data-hook": "review-body"})
+            if body is not None:
+                body = re.sub(regex, '\n', str(body.span))
+                body = body[6:-7] # Remove <span> and </span>
+            data['body'] = body
+
+            # Number of people who liked this review
+            helpful_votes = review.find("span", {"data-hook": "helpful-vote-statement"})
+            if helpful_votes is not None:
+                helpful_votes = helpful_votes
+            data['helpful_votes'] = int(helpful_votes.text.strip().split()[0].replace(',', ''))
+
+            review_data.append(data)
+
+        content['reviews'] = review_data
+    
+    # Now get the next page url, if there is one
+    next_url = None
+    next_url = soup.find("ul", class_="a-pagination")
+    if next_url is not None:
+        next_url = next_url.find("li", class_="a-last")
+        if next_url is not None:
+            next_url = next_url.find("a").attrs['href']
+    return content, next_url
+
+
+if __name__ == '__main__':
+    #soup = init_parser('sample')
+    #results = get_reviews(soup)
+    #print(results)
+    
+    #soup = init_parser('headphones/page_2')
+    #results = get_product_info(soup)
+    #print(results)
+    #print(len(results.keys()))
+
+    # Get the Product Data (Including Customer Reviews)
+    #soup =  init_parser('headphones/B07HZ8JWCL')
+    #results = get_product_data(soup)
+    #print(results)
+
+    # Get the customer reviews alone (https://www.amazon.in/Sony-WH-1000XM3-Wireless-Cancellation-Headphones/product-reviews/B07HZ8JWCL/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&pageNumber=1&reviewerType=all_reviews)
+    soup = init_parser('headphones/reviews_B07HZ8JWCL')
+    results, next_url = get_customer_reviews(soup)
+    #with open('dump_B07HZ8JWCL_reviews.pkl', 'wb') as f:
+    #    pickle.dump(results, f)
+    print(results, next_url)
+
+    # Get the QandA for this product alone (https://www.amazon.in/ask/questions/asin/B07HZ8JWCL/ref=cm_cd_dp_lla_ql_ll#nav-top)
+    #soup = init_parser('headphones/qanda_B07HZ8JWCL')
+    #results, next_url = get_qanda(soup)
+    #print(results, next_url)
---
+++ script.py
@@ -0,0 +1,207 @@
+import os
+import pickle
+import random
+import sys
+import time
+from string import Template
+
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+#from selenium import webdriver
+#from selenium.common.exceptions import NoSuchElementException
+#from webdriver_manager.chrome import ChromeDriverManager
+
+import parse_html
+
+# ua = UserAgent()
+# headers = {'User-Agent': str(ua.chrome)}
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+if len(sys.argv) >= 2:
+	categories = [arg for arg in sys.argv[1:]]
+else:
+	raise ValueError(f'Run the program using: python {sys.argv[0]} CATEGORY1 CATEGORY2 ...')
+
+url_template = Template('https://www.amazon.in/s?k=$category&ref=nb_sb_noss_2')
+
+customer_reviews_template = Template('https://www.amazon.in/review/widgets/average-customer-review/popover/ref=acr_search__popover?ie=UTF8&asin=$PID&ref=acr_search__popover&contextId=search')
+
+
+# Start the session
+session = requests.Session()
+
+
+def scrape_category_listing(categories):
+	# session = requests.Session()
+	server_url = 'https://amazon.in'
+	response = session.get(server_url, headers=headers)
+	assert response.status_code == 200
+	cookies = dict(response.cookies)
+	print(cookies)
+	time.sleep(10)
+
+	final_results = dict()
+
+	for category in categories:
+		final_results[category] = dict()
+		base_url = url_template.substitute(category=category)
+		
+		response = session.get(base_url, headers=headers, cookies=cookies)
+		assert response.status_code == 200
+		if hasattr(response, 'cookies'):
+			cookies = {**cookies, **dict(response.cookies)}
+		time.sleep(5)
+		curr_page = 1
+		curr_url = base_url
+
+		while curr_page < 3:
+			time.sleep(3)
+			html = response.content
+			soup = BeautifulSoup(html, 'html.parser')
+			
+			if not os.path.exists(os.path.join(os.getcwd(), 'data', f'{category}')):
+				os.mkdir(os.path.join(os.getcwd(), 'data', f'{category}'))
+				
+			with open(os.path.join(os.getcwd(), 'data', f'{category}', f'page_{curr_page}.html'), 'wb') as f:
+				f.write(html)
+			
+			# product_map = parse_html.get_product_mapping(soup)
+			
+			product_info = parse_html.get_product_info(soup)
+
+			print(product_info)
+
+			final_results[category][curr_page] = product_info
+
+			# current_page = driver.find_element_by_xpath("//ul[@class='a-pagination']//li[@class='a-selected']/a")
+			
+			page_element = soup.find("ul", class_="a-pagination")
+			
+			if page_element is None:
+				response = session.get(base_url, headers=headers, cookies=cookies)
+				if hasattr(response, 'cookies'):
+					cookies = {**cookies, **dict(response.cookies)}
+				time.sleep(3)
+				break
+			
+			next_page = page_element.find("li", class_="a-last")
+			if next_page is None:
+				response = session.get(base_url, headers=headers, cookies=cookies)
+				if hasattr(response, 'cookies'):
+					cookies = {**cookies, **dict(response.cookies)}
+				time.sleep(3)
+				break
+			
+			page_url = next_page.find("a").attrs['href']
+			
+			print(page_url)
+			
+			response = session.get(server_url + page_url, headers={**headers, 'referer': curr_url}, cookies=cookies)
+			if hasattr(response, 'cookies'):
+				cookies = {**cookies, **dict(response.cookies)}
+			curr_url = server_url + page_url
+			time.sleep(5)
+			curr_page += 1
+			
+		time.sleep(5)
+	return final_results
+
+
+def scrape_product_detail(category, product_url):
+	# session = requests.Session()
+	server_url = 'https://amazon.in'
+	response = session.get(server_url, headers=headers)
+	assert response.status_code == 200
+	cookies = dict(response.cookies)
+	time.sleep(3)
+
+	response = session.get(server_url + product_url, headers=headers, cookies=cookies)
+	if hasattr(response, 'cookies'):
+		cookies = {**cookies, **dict(response.cookies)}
+	time.sleep(10)
+
+	final_results = dict()
+
+	time.sleep(3)
+	html = response.content
+	
+	if not os.path.exists(os.path.join(os.getcwd(), 'data', f'{category}')):
+		os.mkdir(os.path.join(os.getcwd(), 'data', f'{category}'))
+		
+	product_id = parse_html.get_product_id(product_url)
+	print(product_id)
+	
+	with open(os.path.join(os.getcwd(), 'data', f'{category}', f'{product_id}.html'), 'wb') as f:
+		f.write(html)
+	
+	soup = BeautifulSoup(html, 'html.parser')
+
+	# Get the product details
+	details = parse_html.get_product_data(soup)
+	details['product_id'] = product_id # Add the product ID
+	with open(f'dump_{product_id}.pkl', 'wb') as f:
+		pickle.dump(details, f)
+	
+	time.sleep(4)
+	
+	# Get the qanda for this product
+	if 'customer_lazy' in details and details['customer_lazy'] == True:
+		qanda_url = details['customer_qa']
+		response = session.get(qanda_url, headers={**headers, 'referer': server_url + product_url}, cookies=cookies)
+		if hasattr(response, 'cookies'):
+			cookies = {**cookies, **dict(response.cookies)}
+		assert response.status_code == 200
+		time.sleep(5)
+		html = response.content
+		soup = BeautifulSoup(html, 'html.parser')
+		qanda, next_url = parse_html.get_qanda(soup)
+		with open(f'dump_{product_id}_qanda.pkl', 'wb') as f:
+			pickle.dump(qanda, f)
+		print("URL for qand a is " + next_url)
+	
+	# Get the customer reviews
+	if 'customer_reviews' in details and 'reviews_url' in details['customer_reviews']:
+		reviews_url = details['customer_reviews']['reviews_url']
+		response = session.get(server_url + reviews_url, headers={**headers, 'referer': server_url + product_url}, cookies=cookies)
+		if hasattr(response, 'cookies'):
+			cookies = {**cookies, **dict(response.cookies)}
+		assert response.status_code == 200
+		time.sleep(5)
+		html = response.content
+		soup = BeautifulSoup(html, 'html.parser')
+		reviews, next_url = parse_html.get_customer_reviews(soup)
+		with open(f'dump_{product_id}_reviews.pkl', 'wb') as f:
+			pickle.dump(reviews, f)
+		print("URL for customer reviews is " + next_url)
+	
+	time.sleep(3)
+
+	return final_results
+
+
+
+if __name__ == '__main__':
+	#results = scrape_category_listing(categories)
+	#with open('dump.pkl', 'wb') as f:
+	#	pickle.dump(results, f)
+	
+	#print(results)
+	with open('dump.pkl', 'rb') as f:
+		results = pickle.load(f)
+	#print(results)
+	for title in results['mobile'][1]:
+		if results['mobile'][1][title]['product_url'] is not None:
+			product_url = results['mobile'][1][title]['product_url']
+			print(product_url)
+			results = scrape_product_detail('mobile', product_url)
+			print(results)
+			break
+	#results = scrape_product_detail('headphones', '/Sony-WH-1000XM3-Wireless-Cancellation-Headphones/dp/B07HZ8JWCL/ref=sr_1_197?dchild=1&keywords=headphones&qid=1595772158&sr=8-197')
+	#print(results)
+	if False:
+		with open('dump.pkl', 'rb') as f:
+			results = pickle.load(f)
+		print(results)
+	pass
