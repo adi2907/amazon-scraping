a806fc9c1673dcbe68f9f78702754023579d3c6e
Vijay <kuriringohankamehameha@github.com>
2020-09-05 20:13:26 +0530

    Change

--- utils.py
+++ utils.py
@@ -14,14 +14,10 @@ qanda_template = Template('https://www.amazon.in/ask/questions/asin/$PID/$PAGE/r
 
 # List of all the necessary listing page URLS (after applying filters)
 listing_templates = [
-    #Template('https://www.amazon.in/s?k=smartphones&i=electronics&rh=n%3A1805560031%2Cp_89%3AApple%7CNokia%7COnePlus%7COppo%7CPanasonic%7CRedmi%7CSamsung%7CVivo%2Cp_6%3AA14CZOWI0VEHLG&dc&page=$PAGE_NUM&qid=1597662424&rnid=1318474031&ref=sr_pg_$PAGE_NUM'),
     Template('https://www.amazon.in/s?k=smartphone&i=electronics&rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1805560031%2Cp_72%3A1318478031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031&dc&qid=1597812891&rnid=976420031&ref=sr_nr_n_$PAGE_NUM'),
-    Template('https://www.amazon.in/s?k=headphones&i=electronics&rh=n%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_72%3A1318478031&dc&page=$PAGE_NUM&qid=1597664105&rnid=1318475031&ref=sr_pg_$PAGE_NUM'),
-    #Template('https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031&dc&page=$PAGE_NUM&crid=1TGIH58I2LW9I&qid=1597664762&rnid=1318475031&sprefix=ceili%2Caps%2C380&ref=sr_pg_$PAGE_NUM'),
+    Template('https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031&dc&fst=as%3Aoff&qid=1599294897&rnid=3837712031&ref=sr_pg_$PAGE_NUM'),
     Template('https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031%2Cp_n_availability%3A1318485031&dc&crid=1TGIH58I2LW9I&qid=1597813011&rnid=1318483031&sprefix=ceili%2Caps%2C380&ref=sr_nr_p_n_availability_2'),
-    #Template('https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S&dc&page=$PAGE_NUM&qid=1597665403&rnid=1318474031&ref=sr_pg_$PAGE_NUM'),
     Template('https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031&dc&qid=1597813026&rnid=1318483031&ref=sr_nr_p_n_availability_2'),
-    #Template('https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380263031%2Cn%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S&dc&page=$PAGE_NUM&qid=1597665517&rnid=1318474031&ref=sr_pg_$PAGE_NUM'),
     Template('https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380263031%2Cn%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031&dc&qid=1597813042&rnid=1318483031&ref=sr_nr_p_n_availability_2'),
 ]
 
