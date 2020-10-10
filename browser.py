7e39c113cde206b84a7dc58ca17a2aa9b00be975
Vijay <kuriringohankamehameha@github.com>
2020-10-10 20:42:57 +0530

    Update

--- browser.py
+++ browser.py
@@ -1,12 +1,14 @@
+import os
+import time
+from string import Template
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.options import Options
-import time
-import os
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
-
+from utils import listing_categories, listing_templates
 
 ENTRY_URL = "https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753043031&dc&qid=1599327930&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_2"
 
@@ -20,7 +22,113 @@ subcategory = "double door"
 
 # wireless -> Last URL https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_n_feature_six_browse-bin%3A15564047031%7C15564048031&dc&page=36&fst=as%3Aoff&qid=1599907737&rnid=15564019031&ref=sr_pg_35
 
-def run(browser='Firefox'):
+
+def run_category(browser='Firefox'):
+    options = Options()
+    options.headless = True
+    if browser == 'Chrome':
+        # Use chrome
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+    elif browser == 'Firefox':
+        # Set it to Firefox
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+
+    curr = 1
+
+    server_url = 'https://www.amazon.in'
+    
+    try:
+
+        assert len(listing_categories) == len(listing_templates)
+
+        for category, template in zip(listing_categories, listing_templates):
+            url = template.substitute(PAGE_NUM=1)
+            
+            curr = 1
+
+            print(f"GET URL {url}")
+            driver.get(url)
+
+            while True:
+                print(f"At Page Number {curr}")
+                print("Sleeping...")
+                time.sleep(12) # Wait for some time to load
+
+                html = driver.page_source.encode('utf-8', errors='ignore')
+
+
+                try:
+                    captcha = driver.find_element_by_id("captchacharacters")
+                    print("Fuck. Captcha")
+                    time.sleep(10)
+                    driver.get(url)
+                    continue
+                except:
+                    pass
+
+                with open(f'dumps/listing_{category}_{curr}.html', 'wb') as f:
+                    f.write(html)
+
+                print("Written html. Sleeping...")
+                time.sleep(2)
+
+                flag = True
+
+                try:
+                    elements = driver.find_elements_by_class_name("a-last")
+                except:
+                    print("Next page not found. Quitting...")
+                    break
+
+                for element in elements:
+                    try:
+                        #url = element.get_attribute("href")
+                        e = driver.find_element_by_css_selector(".a-last > a:nth-child(1)")
+                        url = e.get_attribute("href")
+                        print(url)
+                        if url is not None:
+                            if not url.startswith(server_url):
+                                url = server_url + url
+                            print(f"URL is {url}")
+                            curr += 1
+                            alpha = 1000
+                            while alpha <= 5000:
+                                try:
+                                    actions = ActionChains(driver)
+                                    driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight-{alpha});")
+                                    time.sleep(5)
+                                    actions.move_to_element(e)
+                                    time.sleep(2)
+                                    actions.click(element).perform()
+                                    time.sleep(5)
+                                    break
+                                except Exception as ex:
+                                    print(ex)
+                                    print(f"Alpha is {alpha}. Now incrementing")
+                                    alpha += 500
+                                    time.sleep(1)
+
+                            print("Went to the next URL")
+                            flag = False
+                            break
+                    except Exception as ex:
+                        print(ex)
+                        continue
+
+                if flag == True:
+                    break
+
+                print("Sleeping...")
+                time.sleep(2)
+        
+    except Exception as ex:
+        print(ex)
+    
+    finally:
+        driver.quit()
+
+
+def run_subcategory(browser='Firefox'):
     #from pyvirtualdisplay import Display
     #display = Display(visible=0, size=(800, 800))
     #display.start()
@@ -135,4 +243,15 @@ def run(browser='Firefox'):
         driver.quit()
 
 if __name__ == '__main__':
-    run()
+    import sys
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'subcategory':
+            print("Subcategory")
+            run_subcategory()
+        elif sys.argv[1] == 'category':
+            print("Category")
+            run_category()
+        else:
+            print("Invalid argument")
+    else:
+        print("Should have one argument")
