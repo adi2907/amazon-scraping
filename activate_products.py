e3379e64a65ce2a1f6e11e039c00a7c6a95f4314
Vijay <kuriringohankamehameha@github.com>
2020-10-11 13:53:47 +0530

    Update

---
+++ activate_products.py
@@ -0,0 +1,54 @@
+import glob
+import os
+
+from bs4 import BeautifulSoup
+
+import parse_data
+import cache
+from utils import listing_categories
+
+
+def main():
+
+    DUMP_DIR = os.path.join(os.getcwd(), 'dumps')
+
+    if not os.path.exists(DUMP_DIR):
+        raise ValueError("Dump Directory doesn't exist")
+
+    cache = cache.Cache()
+    cache.connect('master', use_redis=True)
+
+    categories = listing_categories
+    for category in categories:
+        old_pids = cache.smembers(f"LISTING_{category}_PIDS")
+        
+        # Take a backup
+        for pid in old_pids:
+            cache.sadd(f"LISTING_{category}_PIDS_BACKUP", pid)
+        
+        # Delete old ones
+        cache.delete(f"LISTING_{category}_PIDS")
+
+        # Add the new ones from the dump
+        files = sorted(glob.glob(os.path.join(DUMP_DIR, f"listing_{category}_*")), key=lambda x: int(x.split('.')[0].split('_')[-1]))
+
+        for _, filename in enumerate(files):
+            with open(filename, 'rb') as f:
+                html = f.read()
+
+            soup = BeautifulSoup(html, 'lxml')
+            product_info, _ = parse_data.get_product_info(soup)
+
+            for title in product_info:
+                product_id = product_info[title].get('product_id')
+                if product_id is None:
+                    continue
+                cache.sadd(f"LISTING_{category}_PIDS", product_id)
+        
+        num_pids = len(cache.smembers(f"LISTING_{category}_PIDS"))
+    
+        print(f"Inserted {num_pids} PIDS for category: {category}")
+
+
+if __name__ == '__main__':
+    main()
