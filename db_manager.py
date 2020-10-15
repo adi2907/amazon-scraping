521e0f5b002437a96b34e513dd9a062662f3f7f6
Vijay <kuriringohankamehameha@github.com>
2020-10-15 21:07:55 +0530

    Update

--- db_manager.py
+++ db_manager.py
@@ -1462,7 +1462,7 @@ def index_duplicate_sets(session, table='ProductListing', insert=False, strict=F
             if obj1.product_id == obj2.product_id:
                 continue
             
-            avg_rating_flag = ((obj1.avg_rating == obj2.avg_rating) or (obj1.avg_rating is not None and obj2.avg_rating is not None and abs(obj1.avg_rating - obj2.total_ratings) <= (0.1)))
+            avg_rating_flag = ((obj1.avg_rating == obj2.avg_rating) or (obj1.avg_rating is not None and obj2.avg_rating is not None and abs(obj1.avg_rating - obj2.avg_rating) <= (0.1)))
             rating_flag = ((obj1.total_ratings == obj2.total_ratings) or (obj1.total_ratings is not None and obj2.total_ratings is not None and abs(obj1.total_ratings - obj2.total_ratings) <= (DELTA) * (max(obj1.total_ratings, obj2.total_ratings))))
 
             if (rating_flag and avg_rating_flag):
