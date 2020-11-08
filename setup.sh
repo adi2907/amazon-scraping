19c1f7d548eb4f21ba1fdcf03cf376e63ad5f888
Vijay <kuriringohankamehameha@github.com>
2020-11-08 15:52:34 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install fabric -y
-sudo apt install mysql -y
+sudo apt install mysql-client -y
 sudo apt install python3-pip -y
 sudo apt install -y firefox
 sudo apt install -y redis-server
