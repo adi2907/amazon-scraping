641b91456d082aac5c74a1cfd50891818bc49c98
Vijay <kuriringohankamehameha@github.com>
2020-11-08 14:45:42 +0530

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install mysql -y
+sudo apt install python3-pip -y
+sudo apt install -y firefox
+sudo apt install -y redis-server
+pip3 install -U pip
+git clone https://github.com/almetech/python-scraping
+cd python-scraping
+pip3 install -r requirements.txt
