ed9a6de71c3ad600bb882b3c4b811dd22e6353e6
Vijay <kuriringohankamehameha@github.com>
2020-11-08 15:55:40 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt install mysql-client -y
 sudo apt install python3-pip -y
 sudo apt install -y firefox
 sudo apt install -y redis-server
+ssh -o StrictHostKeyChecking=no git@github.com
 pip3 install -U pip
 git clone https://github.com/almetech/python-scraping
 cd python-scraping
