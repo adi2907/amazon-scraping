65e950e828e84dd74957a891339f20c8ae1271b2
Vijay <kuriringohankamehameha@github.com>
2020-11-08 18:40:26 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ sudo apt install -y redis-server
 pip3 install -U pip
 chmod 600 ~/.ssh/id_rsa
 git clone git@github.com:almetech/python-scraping.git
-cd python-scraping
-pip3 install -r requirements.txt
+pip3 install -r python-scraping/requirements.txt
+echo "Finished setup!"
