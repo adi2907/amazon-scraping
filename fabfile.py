0147516cc0455dcdefeeb5e83ab49962fa952d1b
Vijay <kuriringohankamehameha@github.com>
2020-11-10 00:40:55 +0530

    Update

--- fabfile.py
+++ fabfile.py
@@ -65,8 +65,9 @@ def setup(ctx):
 
         # Now start
         conn.run("tmux new -d -s cron")
-        command = f'cd python-scraping && python3 scrapingtool/archive.py --process_archived_pids --categories "headphones" --instance_id {idx} --num_instances {num_instances} --num_threads 5'
+        command = f'python3 scrapingtool/archive.py --process_archived_pids --categories "headphones" --instance_id {idx} --num_instances {num_instances} --num_threads 5'
         command = command.replace(' ', r'\ ')
+        conn.run(r"tmux send -t cron.0 cd\ python-scraping ENTER")
         conn.run(f"tmux send -t cron.0 {command} ENTER")
         # conn.run(f'cd python-scraping && python3 scrapingtool/archive.py --process_archived_pids --categories "headphones" --instance_id {idx} --num_instances {num_instances} --num_threads 5')
 
