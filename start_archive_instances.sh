dc5d2489bcbd6a954ce950db1636951949150525
Vijay <kuriringohankamehameha@github.com>
2020-11-11 22:01:40 +0530

    Update

--- start_archive_instances.sh
+++ start_archive_instances.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# python3 awstool/api.py --terminate_instances --filename "instance_ids.txt"
+python3 awstool/api.py --terminate_instances --filename "created_instance_ids.txt"
 python3 awstool/api.py --reset_state
 python3 awstool/api.py --create_instance --num_instances 8
 python3 awstool/api.py --get_created_instance_details
