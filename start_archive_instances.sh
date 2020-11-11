c89b885f10f0d82b197a5a644514b942f66a1386
Vijay <kuriringohankamehameha@github.com>
2020-11-11 22:15:55 +0530

    Update

--- start_archive_instances.sh
+++ start_archive_instances.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-NUM_INSTANCES = 8
-
 python3 awstool/api.py --terminate_instances --filename "created_instance_ids.txt"
 python3 awstool/api.py --reset_state
-python3 awstool/api.py --create_instance --num_instances $NUM_INSTANCES
+python3 awstool/api.py --create_instance --num_instances 8
 sleep 300 # Wait for sometime for the instances to activate so that we can get the DNS
 python3 awstool/api.py --get_created_instance_details
 fab setup
