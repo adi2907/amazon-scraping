885770d1415617da9ea81ae64302a0095198132c
Vijay <kuriringohankamehameha@github.com>
2020-11-13 01:52:13 +0530

    Update

--- fabfile.py
+++ fabfile.py
@@ -77,11 +77,11 @@ def setup(ctx):
         conn.run(r"tmux send -t cron.0 cd\ spider ENTER")
         conn.run(f"tmux send -t cron.0 {command} ENTER")
 
-        conn.run("tmux new -d -s monitor")
-        command = f'python3 scrapingtool/monitor.py'
+        conn.run("tmux new -d -s controller")
+        command = f'python3 scrapingtool/controller.py'
         command = command.replace(' ', r'\ ')
-        conn.run(r"tmux send -t monitor.0 cd\ python-scraping ENTER")
-        conn.run(f"tmux send -t monitor.0 {command} ENTER")
+        conn.run(r"tmux send -t controller.0 cd\ python-scraping ENTER")
+        conn.run(f"tmux send -t controller.0 {command} ENTER")
         
         instance_number += 1
         # conn.run(f'cd python-scraping && python3 scrapingtool/archive.py --process_archived_pids --categories "headphones" --instance_id {idx} --num_instances {num_instances} --num_threads 5')
