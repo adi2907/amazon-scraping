8d3b12ebf5b9f9993297c3215d80180495e74c91
Ubuntu <ubuntu@ip-172-31-15-124.ap-south-1.compute.internal>
2020-11-15 18:17:54 +0530

    Update

--- start_archive_instances.sh
+++ start_archive_instances.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 awstool/api.py --terminate_instances --filename "created_instance_ids.txt"
-python3 awstool/api.py --reset_state
-python3 awstool/api.py --create_instance --num_instances 8
+#python3 awstool/api.py --terminate_instances --filename "created_instance_ids.txt"
+#python3 awstool/api.py --reset_state
+#python3 awstool/api.py --create_instance --num_instances 8
+python3 awstool/api.py --start_instances --filename "created_instance_ids.txt"
 sleep 300 # Wait for sometime for the instances to activate so that we can get the DNS
 python3 awstool/api.py --get_created_instance_details
-fab setup
+#fab setup
+fab start-archive
