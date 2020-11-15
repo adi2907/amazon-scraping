0ff736e5cb6b2daeedda8ca8e6c4a36b37ca1003
Vijay <kuriringohankamehameha@github.com>
2020-11-16 00:21:33 +0530

    Update

--- scrapingtool/db_manager.py
+++ scrapingtool/db_manager.py
@@ -1598,7 +1598,7 @@ def finalize_reviews(engine):
     if not os.path.exists(QANDA_COMPRESSED):
         raise ValueError("File not found")
 
-    df = pd.read_csv(QANDA_COMPRESSED, sep=",", encoding="utf-8", lineterminator='\n', usecols=["id", "product_id", "duplicate_set"])
+    df = pd.read_csv(QANDA_COMPRESSED, sep=",", encoding="utf-8", usecols=["id", "product_id", "duplicate_set"])
 
     cleaned_df = df.drop_duplicates(subset=['duplicate_set'], keep='first')
 
