30e32e71b2c7ffabde4d6f37e0e965266ab0b9d3
Ubuntu <ubuntu@ip-172-31-15-124.ap-south-1.compute.internal>
2020-11-17 19:30:38 +0530

    Update

--- create_instances.sh
+++ create_instances.sh
@@ -2,7 +2,13 @@
 
 python3 awstool/api.py --terminate_instances --filename "created_instance_ids.txt"
 python3 awstool/api.py --reset_state
-python3 awstool/api.py --create_instance --num_instances 8
+python scrapingtool/db_manager.py --find_inactive_products
+python3 awstool/api.py --create_instance --filename "num_inactive.txt" --num_instances 5 # 5 is default, incase num_inactive.txt doesn't exist
+
+#python3 awstool/api.py --create_instance --num_instances 8
+
 sleep 300 # Wait for sometime for the instances to activate so that we can get the DNS
+
 python3 awstool/api.py --get_created_instance_details
+
 fab setup
