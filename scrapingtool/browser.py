fa8c076d231799192a86d1557e0d90eeec6a5171
Vijay <kuriringohankamehameha@github.com>
2020-11-24 19:54:54 +0530

    Update

--- scrapingtool/browser.py
+++ scrapingtool/browser.py
@@ -18,8 +18,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 import db_manager
 import parse_data
+from subcategories import subcategory_dict
 from utils import (create_logger, domain_map, domain_to_db, listing_categories,
-                   listing_templates)
+                   listing_templates, is_lambda, category_to_domain)
 
 logger = create_logger('browser')
 
@@ -221,102 +222,107 @@ def run_subcategory(browser='Firefox'):
 
     curr = 1
 
-    server_url = 'https://www.amazon.in'
-
-    category_map = {"headphones": [["wired", "https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_n_feature_six_browse-bin%3A15564046031&dc&fst=as%3Aoff&qid=1599294897&rnid=15564019031&ref=sr_nr_p_n_feature_six_browse-bin_1"], ["wireless", "https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_n_feature_six_browse-bin%3A15564047031%7C15564048031&dc&fst=as%3Aoff&qid=1599295118&rnid=15564019031&ref=sr_nr_p_n_feature_six_browse-bin_1"]]}
-    #category_map = {"washing machine": [["fully automatic", "https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_sixteen_browse-bin%3A2753055031&dc&qid=1599329326&rnid=2753054031&ref=sr_nr_p_n_feature_sixteen_browse-bin_1"], ["semi automatic", "https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_sixteen_browse-bin%3A2753056031&dc&qid=1599329355&rnid=2753054031&ref=sr_nr_p_n_feature_sixteen_browse-bin_2"], ["top load", "https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_fifteen_browse-bin%3A2753052031&dc&qid=1599329495&rnid=2753051031&ref=sr_nr_p_n_feature_fifteen_browse-bin_2"], ["front load", "https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_fifteen_browse-bin%3A2753053031&dc&qid=1599329490&rnid=2753051031&ref=sr_nr_p_n_feature_fifteen_browse-bin_1"]]}
-
-    #category_map = {"refrigerator": [["single door", "https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753044031&dc&qid=1599984176&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_3"]]}
-    
-    #category_map = {"refrigerator": [["multi door", "https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753039031%7C2753045031&dc&qid=1599327851&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_2"], ["direct cool", "https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_eleven_browse-bin%3A2753030031&dc&qid=1599329198&rnid=2753029031&ref=sr_nr_p_n_feature_eleven_browse-bin_1"], ["frost free", "https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_eleven_browse-bin%3A2753031031&dc&qid=1599329267&rnid=2753029031&ref=sr_nr_p_n_feature_eleven_browse-bin_1"]]}
-
     try:
-        for category in category_map:
-            for item in category_map[category]:
-                subcategory, url = item[0], item[1]
-
-                for filename in glob.glob(f"dumps/{category}_{subcategory}_*"):
-                    if os.path.exists(filename):
-                        os.remove(filename)
-                
-                curr = 1
-
-                print(f"GET URL {url}")
-                driver.get(url)
-
-                while True:
-
-                    print(f"At Page Number {curr}")
-                    print("Sleeping...")
-                    time.sleep(12) # Wait for some time to load
-
-                    html = driver.page_source.encode('utf-8', errors='ignore')
-
-
-                    try:
-                        captcha = driver.find_element_by_id("captchacharacters")
-                        print("Fuck. Captcha")
-                        time.sleep(10)
-                        driver.get(url)
-                        continue
-                    except:
+        for category in subcategory_dict:
+            server_url = 'https://' + category_to_domain[category]
+            for subcategory in subcategory_dict[category]:
+                for subcategory_name in subcategory_dict[category][subcategory]:
+                    value = subcategory_dict[category][subcategory][subcategory_name]
+                    url = None
+                    
+                    if isinstance(value, str):
+                        # Url
+                        url = value
+                    elif is_lambda(value):
+                        # Lambda
                         pass
+                    else:
+                        # None
+                        continue
 
-                    with open(f'dumps/{category}_{subcategory}_{curr}.html', 'wb') as f:
-                        f.write(html)
+                    if url is None:
+                        continue
+                    
+                    for filename in glob.glob(f"dumps/{category}_{subcategory_name}_*"):
+                        if os.path.exists(filename):
+                            os.remove(filename)
+                    
+                    curr = 1
 
-                    print("Written html. Sleeping...")
-                    time.sleep(2)
+                    print(f"GET URL {url}")
+                    driver.get(url)
 
-                    flag = True
+                    while True:
 
-                    try:
-                        elements = driver.find_elements_by_class_name("a-last")
-                    except:
-                        print("Next page not found. Quitting...")
-                        break
+                        print(f"At Page Number {curr}")
+                        print("Sleeping...")
+                        time.sleep(12) # Wait for some time to load
 
-                    for element in elements:
+                        html = driver.page_source.encode('utf-8', errors='ignore')
+
                         try:
-                            #url = element.get_attribute("href")
-                            e = driver.find_element_by_css_selector(".a-last > a:nth-child(1)")
-                            url = e.get_attribute("href")
-                            print(url)
-                            if url is not None:
-                                if not url.startswith(server_url):
-                                    url = server_url + url
-                                print(f"URL is {url}")
-                                curr += 1
-                                alpha = 1000
-                                while alpha <= 5000:
-                                    try:
-                                        actions = ActionChains(driver)
-                                        driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight-{alpha});")
-                                        time.sleep(5)
-                                        actions.move_to_element(e)
-                                        time.sleep(2)
-                                        actions.click(element).perform()
-                                        time.sleep(5)
-                                        break
-                                    except Exception as ex:
-                                        print(ex)
-                                        print(f"Alpha is {alpha}. Now incrementing")
-                                        alpha += 500
-                                        time.sleep(1)
-
-                                print("Went to the next URL")
-                                flag = False
-                                break
-                        except Exception as ex:
-                            print(ex)
+                            captcha = driver.find_element_by_id("captchacharacters")
+                            print("Fuck. Captcha")
+                            time.sleep(10)
+                            driver.get(url)
                             continue
+                        except:
+                            pass
 
-                    if flag == True:
-                        break
+                        with open(f'dumps/{category}_{subcategory_name}_{curr}.html', 'wb') as f:
+                            f.write(html)
 
-                    print("Sleeping...")
-                    time.sleep(2)
-        
+                        print("Written html. Sleeping...")
+                        time.sleep(2)
+
+                        flag = True
+
+                        try:
+                            elements = driver.find_elements_by_class_name("a-last")
+                        except:
+                            print("Next page not found. Quitting...")
+                            break
+
+                        for element in elements:
+                            try:
+                                #url = element.get_attribute("href")
+                                e = driver.find_element_by_css_selector(".a-last > a:nth-child(1)")
+                                url = e.get_attribute("href")
+                                print(url)
+                                if url is not None:
+                                    if not url.startswith(server_url):
+                                        url = server_url + url
+                                    print(f"URL is {url}")
+                                    curr += 1
+                                    alpha = 1000
+                                    while alpha <= 5000:
+                                        try:
+                                            actions = ActionChains(driver)
+                                            driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight-{alpha});")
+                                            time.sleep(5)
+                                            actions.move_to_element(e)
+                                            time.sleep(2)
+                                            actions.click(element).perform()
+                                            time.sleep(5)
+                                            break
+                                        except Exception as ex:
+                                            print(ex)
+                                            print(f"Alpha is {alpha}. Now incrementing")
+                                            alpha += 500
+                                            time.sleep(1)
+
+                                    print("Went to the next URL")
+                                    flag = False
+                                    break
+                            except Exception as ex:
+                                print(ex)
+                                continue
+
+                        if flag == True:
+                            break
+
+                        print("Sleeping...")
+                        time.sleep(2)
         driver.quit()
     except Exception as ex:
         print(ex)
---
+++ scrapingtool/subcategories.py
@@ -0,0 +1,79 @@
+subcategory_dict = {
+    'headphones': {
+        'Type': {
+            'wired': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_n_feature_six_browse-bin%3A15564046031&dc&fst=as%3Aoff&qid=1599294897&rnid=15564019031&ref=sr_nr_p_n_feature_six_browse-bin_1',
+            'wireless': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_n_feature_six_browse-bin%3A15564047031%7C15564048031&dc&fst=as%3Aoff&qid=1599295118&rnid=15564019031&ref=sr_nr_p_n_feature_six_browse-bin_1',
+            'tws': lambda title: True if any(token in title.lower() for token in ['tws', 'true wireless', 'true-wireless', 'truly wireless', 'truly-wireless']) else False,
+        },
+        'Price': {
+            "<500": 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A-50000&dc&fst=as%3Aoff&qid=1599295515&ref=sr_ex_p_n_feature_six_brow_0',
+            '500-1000': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A50000-100000&dc&fst=as%3Aoff&qid=1599295566&rnid=1318502031&ref=sr_nr_p_36_1',
+            '1000-2000':'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A100000-200000&dc&fst=as%3Aoff&qid=1599295865&rnid=1318502031&ref=sr_nr_p_36_5',
+            '2000-3000': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A200000-300000&dc&fst=as%3Aoff&qid=1599295874&rnid=1318502031&ref=sr_nr_p_36_1',
+            '3000-5000': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A300000-500000&dc&fst=as%3Aoff&qid=1599295929&rnid=1318502031&ref=sr_nr_p_36_5',
+            '>5000': 'https://www.amazon.in/s?i=electronics&bbn=1388921031&rh=n%3A976419031%2Cn%3A976420031%2Cn%3A1388921031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_72%3A1318478031%2Cp_36%3A500000-5000000&dc&fst=as%3Aoff&qid=1599296034&rnid=1318502031&ref=sr_nr_p_36_5',
+        },
+    },
+    'smartphones': {
+        'Price': {
+            'budget (<10000)': 'https://www.amazon.in/s?k=smartphone&i=electronics&rh=n%3A1805560031%2Cp_72%3A1318478031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_36%3A1318505031&dc&qid=1604122900&rnid=1318502031&ref=sr_nr_p_36_1',
+            'economy (10000-20000)': 'https://www.amazon.in/s?k=smartphone&i=electronics&rh=n%3A1805560031%2Cp_72%3A1318478031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_36%3A1000000-2000000&dc&qid=1604122922&rnid=1318502031&ref=sr_nr_p_36_1',
+            'mid premium (20000-30000)': 'https://www.amazon.in/s?k=smartphone&i=electronics&rh=n%3A1805560031%2Cp_72%3A1318478031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_36%3A2000000-3000000&dc&qid=1604122997&rnid=1318502031&ref=sr_nr_p_36_1',
+            'premium (>30000)': 'https://www.amazon.in/s?k=smartphone&i=electronics&rh=n%3A1805560031%2Cp_72%3A1318478031%2Cp_6%3AA14CZOWI0VEHLG%2Cp_n_availability%3A1318485031%2Cp_36%3A3000000-9000000&dc&qid=1604123115&rnid=1318502031&ref=sr_pg_1',
+        },
+    },
+    'ceiling fan': {
+        'Price': {
+            'economy (<1500)': 'https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031%2Cp_n_availability%3A1318485031%2Cp_36%3A-150000&dc&crid=1TGIH58I2LW9I&qid=1604125306&rnid=3444809031&sprefix=ceili%2Caps%2C380&ref=sr_nr_p_36_3',
+            'standard (1500-2500)': 'https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031%2Cp_n_availability%3A1318485031%2Cp_36%3A150000-250000&dc&crid=1TGIH58I2LW9I&qid=1604125324&rnid=3444809031&sprefix=ceili%2Caps%2C380&ref=sr_nr_p_36_1',
+            'premium (2500-4000)': 'https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031%2Cp_n_availability%3A1318485031%2Cp_36%3A250000-400000&dc&crid=1TGIH58I2LW9I&qid=1604125340&rnid=3444809031&sprefix=ceili%2Caps%2C380&ref=sr_nr_p_36_2',
+            'luxury (>4000)': 'https://www.amazon.in/s?k=ceiling+fan&i=kitchen&rh=n%3A2083427031%2Cn%3A4369221031%2Cp_6%3AAT95IG9ONZD7S%2Cp_72%3A1318478031%2Cp_n_availability%3A1318485031%2Cp_36%3A400000-2000000&dc&crid=1TGIH58I2LW9I&qid=1604125356&rnid=3444809031&sprefix=ceili%2Caps%2C380&ref=sr_nr_p_36_2',
+        },
+        'Features': {
+            'bldc': lambda title: True if 'bldc' in title.lower() else False,
+            'smart': lambda features: True if any(token in features.lower() for token in ['remote', 'bldc', 'smart', 'iot']) else False,
+            'lights': lambda title: True if any(token in title.lower() for token in ['light', 'lights', 'decorative']) else False,
+        }
+    },
+    'refrigerator': {
+        'Capacity': {
+            '<120': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480496031&dc&qid=1599326565&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_1',
+            '120-200': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480497031&dc&qid=1599327453&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_2',
+            '200-230': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480498031&dc&qid=1599327541&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_3',
+            '230-300': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480499031&dc&qid=1599327668&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_4',
+            '300-400': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480500031&dc&qid=1604128869&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_6',
+            '>400': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480501031&dc&qid=1604128890&rnid=1480495031&ref=sr_nr_p_n_feature_seven_browse-bin_5',
+        },
+        'Door': {
+            'multi door': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753039031%7C2753045031&dc&qid=1599327851&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_2',
+            'double Door': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753043031&dc&qid=1599327930&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_2',
+            'single Door': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_thirteen_browse-bin%3A2753044031&dc&qid=1604128967&rnid=2753038031&ref=sr_nr_p_n_feature_thirteen_browse-bin_4',
+        },
+        'Defrost': {
+            'direct cool': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_eleven_browse-bin%3A2753030031&dc&qid=1599329198&rnid=2753029031&ref=sr_nr_p_n_feature_eleven_browse-bin_1',
+            'frost free': 'https://www.amazon.in/s?k=refrigerator&i=kitchen&rh=n%3A1380365031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_eleven_browse-bin%3A2753031031&dc&qid=1604129131&rnid=2753029031&ref=sr_nr_p_n_feature_eleven_browse-bin_1',
+        },
+    },
+    'washing machine': {
+        'Automatic': {
+            'semi automatic': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_sixteen_browse-bin%3A2753056031&dc&qid=1599329355&rnid=2753054031&ref=sr_nr_p_n_feature_sixteen_browse-bin_2',
+            'fully automatic': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_sixteen_browse-bin%3A2753055031&dc&qid=1599329326&rnid=2753054031&ref=sr_nr_p_n_feature_sixteen_browse-bin_1',
+        },
+        'Loading': {
+            'front load': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_fifteen_browse-bin%3A2753053031&dc&qid=1599329490&rnid=2753051031&ref=sr_nr_p_n_feature_fifteen_browse-bin_1',
+            'top load': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_fifteen_browse-bin%3A2753052031&dc&qid=1599329495&rnid=2753051031&ref=sr_nr_p_n_feature_fifteen_browse-bin_2',
+        },
+        'Capacity': {
+            '<7kg': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480508031%7C1480509031&dc&qid=1599329576&rnid=1480507031&ref=sr_nr_p_n_feature_seven_browse-bin_2',
+            '7-8kg': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480510031&dc&qid=1599329628&rnid=1480507031&ref=sr_nr_p_n_feature_seven_browse-bin_3',
+            '>=8kg': 'https://www.amazon.in/s?k=washing+machine&i=kitchen&rh=n%3A1380369031%2Cp_72%3A1318478031%2Cp_6%3AAT95IG9ONZD7S%2Cp_n_availability%3A1318485031%2Cp_n_feature_seven_browse-bin%3A1480511031&dc&qid=1599329662&rnid=1480507031&ref=sr_nr_p_n_feature_seven_browse-bin_4',
+        },
+        'Price': {
+            '<10000': lambda curr_price: True if curr_price < 10000 else False,
+            '10000-15000': lambda curr_price: True if ((curr_price >= 10000) and (curr_price < 15000)) else False,
+            '15000-20000': lambda curr_price: True if ((curr_price >= 15000) and (curr_price < 20000)) else False,
+            '20000-30000': lambda curr_price: True if ((curr_price >= 20000) and (curr_price < 30000)) else False,
+            '>30000': lambda curr_price: True if curr_price >= 30000 else False,
+        }
+    }
+}
