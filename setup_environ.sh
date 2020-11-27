6979729baf0f7fcff0a3ad5bc63247e0925b4605
Vijay <42909864+kuriringohankamehameha@users.noreply.github.com>
2020-11-27 20:29:05 +0530

    Update setup_environ.sh

--- setup_environ.sh
+++ setup_environ.sh
@@ -4,6 +4,10 @@ cp .env spider/.env
 cp .env backend/amazonscraper/.env
 
 cp categories.json backend/amazonscraper/categories.json
+cp categories.json spider/categories.json
+
 cp scrapingtool/subcategories.py backend/amazonscraper/subcategories.py
 cp scrapingtool/subcategories.py spider/scrapingtool/subcategories.py
-cp categories.json spider/categories.json
+
+cp parameters.csv backend/parameters.csv
+cp parameters.csv spider/parameters.csv
